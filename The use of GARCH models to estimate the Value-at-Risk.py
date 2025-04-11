25461f2dc9d57316319ac9dd5738664baa4c7d46
Giorgio Pession Borghi <145056350+GioPess@users.noreply.github.com>
2025-04-11 14:51:20 +0200

    Delete The use of GARCH models to estimate the Value-at-Risk.py

--- The use of GARCH models to estimate the Value-at-Risk.py
+++
@@ -1,8 +0,0 @@
-# The use of GARCH models to estimate the Value-at-Risk
-
-pip install numpy pandas yfinance arch matplotlib
-
-import yfinance as yf
-
-data = yf.download("^GSPC", start = "2018-01-01", end = "2024-01-01")
-data = data["Adj Close"]
